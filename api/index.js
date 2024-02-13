c7572a8c40d6f0f98c3a2bae1323f002101d4ffd
amir <amirlearn08@gmail.com>
2024-02-13 05:54:30 -0800

    create error middle ware

--- api/index.js
+++ api/index.js
@@ -20,6 +20,16 @@ mongoose.connect(
 
 app.use("/api/users",userRouter)
 app.use("/api/auth",authRouter)
+
+app.use((err,req,res,next)=>{
+const statusCode  = err.statusCode || 500 
+const message  = err.message || 'Internal Server Error'
+res.status(statusCode).json({
+  success:false,
+  statusCode,
+  message
+})
+})
 app.listen(1010, () => {
   console.log(`server is running on port 1010`);
 });
