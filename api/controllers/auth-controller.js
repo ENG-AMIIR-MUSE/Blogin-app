7324b7a6a6871ae32e6a390ce1e7be2465f8400b
amir <amirlearn08@gmail.com>
2024-02-17 07:44:05 -0800

    encrypted password from the clinet

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -3,34 +3,6 @@ import bycrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import  Jwt  from "jsonwebtoken";
 
-// export const signUp = async (req, res, next) => {
-//   try {
-//     const { username, email, password } = req.body;
-//     console.log(username)
-//     if (
-//       !username ||
-//       !email ||
-//       !password ||
-//       username == "" ||
-//       email == "" ||
-//       password == ""
-//     ) {
-//       return next(errorHandler(400, "All Fields Are Required"));
-//     }
-//     const hashedPassword = bycrypt.hashSync(password, 10);
-
-//     // create user
-//     const newUser = new userModel({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-//     await newUser.save();
-//     res.status(200).json("Signup Successfully");
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -66,7 +38,7 @@ export const signUp = async (req, res, next) => {
 
 
 export const signIn = async (req, res, next) => {
-  console.log("insdide the sigin ")
+
   const { email, password } = req.body;
 
   if (!email || !password || email == "" || password == "") {
@@ -79,13 +51,14 @@ export const signIn = async (req, res, next) => {
       return next(errorHandler(404, "User Not Found"));
     }
     const isPasswordValid = bycrypt.compareSync(password, isValidUser.password);
-    console.log(isPasswordValid);
+    
     if (!isPasswordValid) {
       return next(errorHandler(400, "Invalid Password"));
     }
 
     const token = Jwt.sign({ Id: isValidUser._id }, process.env.JWT_SECRET);
-    return res.status(200).cookie("access_token", token, { httpOnly:true }).json(isValidUser);
+    const {password:pass, ...rest} =  isValidUser._doc 
+    return res.status(200).cookie("access_token", token, { httpOnly:true }).json(rest);
   } catch (error) {
     console.log(error)
   }
